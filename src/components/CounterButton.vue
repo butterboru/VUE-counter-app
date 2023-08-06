4b64ef07933fa0494ea1f173657d43e6198d09e7
butterboru <bandersnacth2000@gmail.com>
2023-08-05 18:33:07 -0700

    2nd commit

---
+++ src/components/CounterButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    {{ counter }}
+ 
+    <div>
+      <p></p>
+      <button @click="pressAndDecrease()">-</button>
+      <button @click="pressAndIncrease()">+</button> 
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const previousCount = localStorage.getItem('key') || 0
+const counter = ref(previousCount)
+
+const pressAndDecrease = () => {
+  counter.value--
+  localStorage.setItem('key', counter.value)
+}
+
+const pressAndIncrease = () => {
+  counter.value++
+  localStorage.setItem('key', counter.value)
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Page2View.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    {{ counter }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const previousCount = localStorage.getItem('key') || 0
+const counter = ref(previousCount)
+
+</script>
